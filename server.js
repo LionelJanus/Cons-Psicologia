83847bc8cb3161a1fd7a80bbda99dd4a8d07ddd7
Lionel Januszevski <ljanuszevski@HCJSM.local>
2024-09-17 13:04:54 -0300

    mejoras en la estetica

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5500'
+  origin: 'http://127.0.0.1:5000'
 }));
 
 app.use(express.json());
